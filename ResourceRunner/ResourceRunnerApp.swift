5d533992f55258fe93240fc56a438461c11ba9e6
zipkero <zipkero@gmail.com>
2026-08-02 11:16:45 +0900

    Initialize ResourceRunner

---
+++ ResourceRunner/ResourceRunnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResourceRunnerApp.swift
+//  ResourceRunner
+//
+//  Created by zipkero on 8/2/26.
+//
+
+import SwiftUI
+
+@main
+struct ResourceRunnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ResourceRunnerTests/ResourceRunnerTests.swift
@@ -0,0 +1,19 @@
+//
+//  ResourceRunnerTests.swift
+//  ResourceRunnerTests
+//
+//  Created by zipkero on 8/2/26.
+//
+
+import Testing
+@testable import ResourceRunner
+
+struct ResourceRunnerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
